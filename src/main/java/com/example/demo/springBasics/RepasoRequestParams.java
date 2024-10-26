b0b5f8e1656750ff53fd802dc460d33cd44c07ae
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2024-10-26 05:47:40 -0300

    agregando mas practicas

--- src/main/java/com/example/demo/springBasics/RepasoRequestParams.java
+++ src/main/java/com/example/demo/springBasics/RepasoRequestParams.java
@@ -6,15 +6,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RepasoRequestParams {
-
     @GetMapping("/1")
-//    RequestPAram permite desde el url con notacion ?valor=..&valor2=.. definir un valor desde la url
+
+//    @RequestParam  desde el url con notacion /url?valor=..&valor2=..
+//    permite definir un valor desde la url
 //    esto permite hacer muchas cosas como operaciones
     public String cosas_a_Repasar(@RequestParam String algo, int data){
 
         String saludo = "Mi nombre es Rodrigo y estoy escribiendo un texto";
-
         return  "Buenos dias" + algo + data;
     }
 
 }
+
+
+
+
+
+
+
+
+
+
